91fdc9f5a9a6a48ac163aca9dec50f7b3068c557
Harshid Vaddoriya <harshidpatel388@gmail.com>
2023-07-12 10:42:33 +0530

    complete

--- Coustom MenuBar/ViewController.swift
+++ Coustom MenuBar/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+   
+    @IBOutlet weak var menuView: UIView!
+    
+    @IBOutlet weak var xValue: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func buttonAction(_ sender: UIButton) {
+        xValue.constant = 0
+    }
+    
+    @IBAction func backButtonAction(_ sender: UIButton) {
+        xValue.constant = -280
+    }
 }
 
